b5f89bbfe3a5e11ec91f2982dbabfa8edababae1
Rspillere <rspillere@gmail.com>
2023-05-19 14:27:43 -0300

    ORM added

--- services/product.service.js
+++ services/product.service.js
@@ -1,11 +1,15 @@
 const { fakerDE: faker } = require('@faker-js/faker');
 const boom = require('@hapi/boom');
 
+const sequelize = require('./../libs/sequelize')
+
 class ProductsService {
 
   constructor(){
     this.products = [];
     this.generate();
+    // this.pool = pool;
+    // this.pool.on('error', (err)=> console.log(err));
   }
 
   generate() {
@@ -30,8 +34,13 @@ class ProductsService {
     return newProduct;
   }
 
-  find() {
-    return this.products;
+  async find() {
+    const query = 'SELECT * FROM tasks';
+    const [data] = await sequelize.query(query)
+
+    //const rta = await this.pool.query(query)
+    //return rta.rows;
+    return data;
   }
 
   async findOne(id) {
